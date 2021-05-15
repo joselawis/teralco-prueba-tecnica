7a692e4f9a7412876020c0773507fa4756aecdad
joselawis <joselawis@gmail.com>
2021-05-15 11:41:18 +0200

    Database creation

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS MARCA;
+
+CREATE TABLE MARCA (
+    id int auto_increment primary key,
+    name varchar(250) not null
+);
+
+DROP TABLE IF EXISTS COCHE;
+
+CREATE TABLE COCHE (
+    id int auto_increment primary key,
+    name varchar(250) not null,
+    color varchar(250) not null,
+    marca_id int not null
+);
+
+ALTER TABLE COCHE ADD FOREIGN KEY (marca_id) REFERENCES MARCA(id);
+
+DROP TABLE IF EXISTS PRECIO;
+
+CREATE TABLE PRECIO (
+    id int auto_increment primary key,
+    startDate timestamp not null,
+    endDate timestamp not null,
+    price double not null,
+    coche_id int not null
+);
+
+ALTER TABLE PRECIO ADD FOREIGN KEY (coche_id) REFERENCES COCHE(id);
