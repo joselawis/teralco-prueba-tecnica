c021ab48426dd457c83d7ea93c2332b2cdd38ccc
joselawis <joselawis@gmail.com>
2021-05-15 12:22:00 +0200

    Untrack files in .gitignore

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,30 +2,35 @@ DROP TABLE IF EXISTS PRECIO;
 DROP TABLE IF EXISTS COCHE;
 DROP TABLE IF EXISTS MARCA;
 
-CREATE TABLE MARCA (
-    id int auto_increment primary key,
+CREATE TABLE MARCA
+(
+    id   int auto_increment primary key,
     name varchar(250) not null
 );
 
 
 
-CREATE TABLE COCHE (
-    id int auto_increment primary key,
-    name varchar(250) not null,
-    color varchar(250) not null,
-    marca_id int not null
+CREATE TABLE COCHE
+(
+    id       int auto_increment primary key,
+    name     varchar(250) not null,
+    color    varchar(250) not null,
+    marca_id int          not null
 );
 
-ALTER TABLE COCHE ADD FOREIGN KEY (marca_id) REFERENCES MARCA(id);
+ALTER TABLE COCHE
+    ADD FOREIGN KEY (marca_id) REFERENCES MARCA (id);
 
 
 
-CREATE TABLE PRECIO (
-    id int auto_increment primary key,
+CREATE TABLE PRECIO
+(
+    id        int auto_increment primary key,
     startDate timestamp not null,
-    endDate timestamp not null,
-    price double not null,
-    coche_id int not null
+    endDate   timestamp not null,
+    price     double    not null,
+    coche_id  int       not null
 );
 
-ALTER TABLE PRECIO ADD FOREIGN KEY (coche_id) REFERENCES COCHE(id);
+ALTER TABLE PRECIO
+    ADD FOREIGN KEY (coche_id) REFERENCES COCHE (id);
